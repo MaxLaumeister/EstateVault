e0176b47f116d627ad90db340911893fa7141b54
Maximillian Laumeister <max@maxlaumeister.com>
2019-10-24 14:50:05 -0700

    More functionality, with tests

--- contracts/LockBoxController.sol
+++ contracts/LockBoxController.sol
@@ -13,11 +13,11 @@ contract LockBoxController is ERC721 {
     struct LockBox {
         address childContractAddress;
         uint checkInInterval;
-        uint releaseTime;
+        uint lastCheckIn;
         address beneficiary;
     }
 
-    LockBox[] public lockBoxes;
+    LockBox[] public lockboxes;
 
     constructor() public {
         // Global setup
@@ -26,36 +26,55 @@ contract LockBoxController is ERC721 {
     function newLockBox() public {
         // Create instance of the child contract, with this contract as the parent
         LockBoxChildContract childContract = new LockBoxChildContract(address(this));
-        uint256 lockboxId = lockBoxes.length;
+        uint256 lockboxId = lockboxes.length;
         _mint(msg.sender, lockboxId); // Create a new ERC-721 token with sequential ID
-        lockBoxes.push(LockBox(address(childContract), 365 days, block.timestamp + 365 days, address(0)));
+        lockboxes.push(LockBox(address(childContract), 365 days, block.timestamp + 365 days, address(0)));
     }
 
-    // As beneficiary, transfer ownership to an address of your choice
-    function claimAsBeneficiary(address from, address to, uint256 lockboxId) public {
+    // As beneficiary, transfer ownership to an address of your choice. Zeroes out the beneficiary.
+    function claimOwnershipAsBeneficiary(uint256 lockboxId, address from, address to) public {
         require(_isAuthorizedBeneficiary(lockboxId));
         _transferFrom(from, to, lockboxId);
+        lockboxes[lockboxId].beneficiary = address(0); // Zero out beneficiary
+    }
+
+    function setBeneficiary(uint256 lockboxId, address beneficiary) public {
+        require(_isOwner(lockboxId));
+        lockboxes[lockboxId].beneficiary = beneficiary;
+    }
+
+    function setCheckInInterval(uint256 lockboxId, uint newCheckInInterval) public {
+        require(_isOwner(lockboxId));
+        lockboxes[lockboxId].checkInInterval = newCheckInInterval;
+        lockboxes[lockboxId].lastCheckIn = block.timestamp;
+    }
+
+    function checkIn(uint256 lockboxId) public {
+        require(_isOwner(lockboxId));
+        lockboxes[lockboxId].lastCheckIn = block.timestamp;
     }
 
     function transferERC20(uint256 lockboxId, address tokenContractAddress, address recipient, uint256 amount) public {
         require(_isOwner(lockboxId));
-        LockBoxChildContract childContract = LockBoxChildContract(lockBoxes[lockboxId].childContractAddress);
+        LockBoxChildContract childContract = LockBoxChildContract(lockboxes[lockboxId].childContractAddress);
         childContract.transferERC20(tokenContractAddress, recipient, amount);
     }
 
     function transferERC721(uint256 lockboxId, address tokenContractAddress, address recipient, uint256 tokenId) public {
         require(_isOwner(lockboxId));
-        LockBoxChildContract childContract = LockBoxChildContract(lockBoxes[lockboxId].childContractAddress);
+        LockBoxChildContract childContract = LockBoxChildContract(lockboxes[lockboxId].childContractAddress);
         childContract.transferERC721(tokenContractAddress, recipient, tokenId);
     }
 
     function _isAuthorizedBeneficiary(uint256 lockboxId) private view returns (bool) {
         // Beneficiary is only allowed if the timelock is up
-        LockBox memory lockbox = lockBoxes[lockboxId];
-        return msg.sender == lockbox.beneficiary && block.timestamp >= lockbox.releaseTime;
+        LockBox memory lockbox = lockboxes[lockboxId];
+        return msg.sender == lockbox.beneficiary && block.timestamp >= lockbox.lastCheckIn + lockbox.checkInInterval; // No need for safemath, because these variables were set sanely by the owner
     }
 
     function _isOwner(uint256 lockboxId) private view returns (bool) {
         return msg.sender == ownerOf(lockboxId);
     }
+
+    // TODO: Emit events
 }
--- test/estatewallet.js
+++ test/estatewallet.js
@@ -1,5 +1,6 @@
 const LockBoxController = artifacts.require("LockBoxController");
 const LockBoxChildContract = artifacts.require("LockBoxChildContract");
+const truffleAssert = require('truffle-assertions');
 
 const NUM_CONTRACTS = 3; // Do not edit - address 4 and greater are used for additional tests
 
@@ -25,43 +26,80 @@ contract("LockBoxController", async function(accounts) {
     it("can create " + NUM_CONTRACTS + " properly-owned lockboxes with sequential IDs", async function() {
         for (let i = 0; i < NUM_CONTRACTS; i++) {
             await lockBoxControllerInstance.newLockBox({ from: accounts[i] });
-            // console.log("Child address: ", (await lockBoxControllerInstance.lockBoxes(i)).childContractAddress);
+            // console.log("Child address: ", (await lockBoxControllerInstance.lockboxes(i)).childContractAddress);
             assert(await lockBoxControllerInstance.ownerOf(i) == accounts[i]);
         }
     });
 
     it("has " + NUM_CONTRACTS + " new child contracts that point back to it", async function() {
         for (let i = 0; i < NUM_CONTRACTS; i++) {
-            let childaddress = (await lockBoxControllerInstance.lockBoxes(i)).childContractAddress;
+            let childaddress = (await lockBoxControllerInstance.lockboxes(i)).childContractAddress;
             let childcontract = await LockBoxChildContract.at(childaddress);
             // console.log((await childcontract._parentContract()), lockBoxControllerInstance.address);
             assert((await childcontract._parentContract()) == lockBoxControllerInstance.address);
         }
     });
 
-    /*
+    it("beneficiary starts as zero address", async function() {
+        assert((await lockBoxControllerInstance.lockboxes(0)).beneficiary == 0);
+    });
+
+    
     it("owner can transfer ownership of lockbox", async function() {
-        
+        await lockBoxControllerInstance.safeTransferFrom(accounts[0], accounts[3], 0 /* ERC721 token id */, { from: accounts[0] });
+        assert(await lockBoxControllerInstance.ownerOf(0) == accounts[3]);
     });
 
-    it("owner of different lockbox cannot transfer ownership of lockbox", async function() {
-        
+    it("stranger cannot transfer ownership of lockbox", async function() {
+        await truffleAssert.reverts(lockBoxControllerInstance.safeTransferFrom(accounts[3], accounts[4], 0 /* ERC721 token id */, { from: accounts[0] }));
+        await truffleAssert.reverts(lockBoxControllerInstance.safeTransferFrom(accounts[3], accounts[4], 0 /* ERC721 token id */, { from: accounts[2] }));
+        await truffleAssert.reverts(lockBoxControllerInstance.safeTransferFrom(accounts[3], accounts[4], 0 /* ERC721 token id */, { from: accounts[4] }));
     });
 
-    it("anyone can send an ERC-20 token and an ERC-721 token to any lockbox contract", async function() {
+    //it("anyone can send ETH to any lockbox contract", async function() {
         
+    //});
+
+    //it("anyone can send ERC-20 tokens to any lockbox contract", async function() {
+        
+    //});
+
+    //it("anyone can send ERC-721 tokens to any lockbox contract", async function() {
+        
+    //});
+
+    //it("owner can transfer ", async function() {
+        
+    //});
+
+    it("owner can set beneficiary", async function() {
+        await lockBoxControllerInstance.setBeneficiary(1, accounts[4], { from: accounts[1] });
+        assert((await lockBoxControllerInstance.lockboxes(1)).beneficiary == accounts[4]);
     });
 
-    it("beneficiary cannot yet transfer ownership of lockbox", async function() {
-        
+    it("beneficiary cannot yet claim ownership of lockbox", async function() {
+        await truffleAssert.reverts(lockBoxControllerInstance.claimOwnershipAsBeneficiary(1, accounts[1], accounts[4], { from: accounts[4] }));
     });
+
+    //it("beneficiary cannot transfer ETH or tokens away from lockbox", async function() {
+        
+    //});
 
     it("owner can change check-in period of lockbox (to zero)", async function() {
-        
+        await lockBoxControllerInstance.setCheckInInterval(1, 0, { from: accounts[1] });
     });
 
-    it("beneficiary can now transfer ownership of lockbox", async function() {
-        
+    it("beneficiary can now claim ownership of lockbox", async function() {
+        await lockBoxControllerInstance.claimOwnershipAsBeneficiary(1, accounts[1], accounts[4], { from: accounts[4] });
+        assert(await lockBoxControllerInstance.ownerOf(1) == accounts[4]);
     });
-    */
+
+    it("after beneficiary claims ownership, the beneficiary is unset to the zero address", async function() {
+        assert((await lockBoxControllerInstance.lockboxes(1)).beneficiary == 0);
+    });
+
+    //it("beneficiary (new owner) can now transfer ETH and tokens away from child contract", async function() {
+        
+    //});
+    
 });
