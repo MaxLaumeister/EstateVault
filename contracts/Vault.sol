132179a1f7b69764ad63ff9093e5ff031cab4602
Maximillian Laumeister <max@maxlaumeister.com>
2019-12-01 14:39:52 -0800

    Progress, more tests passing

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -10,11 +10,6 @@ contract Vault is ERC721Holder  {
     VaultManager public _parentContract;
     uint256 public _vaultId;
 
-    modifier onlyParentContract() {
-         require(msg.sender == address(_parentContract), "functions can only be called from the main VaultManager contract");
-         _;
-    }
-
     function () external payable {} // This contract acts like a wallet, holding ETH, ERC-20 and ERC-721
 
     constructor (VaultManager parentContract, uint256 vaultId) public {
@@ -38,10 +33,5 @@ contract Vault is ERC721Holder  {
         require(_parentContract.isOwner(_vaultId), "only the owner of this vault's key can withdraw ETH");
         recipient.transfer(amount); // This potentially passes control to an external contract
     }
-
-    //function removeBeneficiary(uint256 vaultId) public {
-    //    require(isOwner(vaultId), "only owner can remove beneficiary");
-        // TODO: Yank beneficiary ticket back into the vault
-    //}
 
 }
