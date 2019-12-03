394d0260144608840d36cd1e90426ce3f87804d6
Maximillian Laumeister <max@maxlaumeister.com>
2019-12-02 18:34:40 -0800

    You guessed it... more tests passing

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -3,34 +3,32 @@ pragma solidity 0.5.12;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721Holder.sol";
-import "./VaultManager.sol";
+import "./VaultKey.sol";
 
 contract Vault is ERC721Holder  {
 
-    VaultManager public _parentContract;
+    VaultKey public _vaultKeyTokenContract;
     uint256 public _vaultId;
 
     function () external payable {} // This contract acts like a wallet, holding ETH, ERC-20 and ERC-721
 
-    constructor (VaultManager parentContract, uint256 vaultId) public {
-        _parentContract = parentContract;
+    constructor (VaultKey vaultKeyTokenContract, uint256 vaultId) public {
+        _vaultKeyTokenContract = vaultKeyTokenContract;
         _vaultId = vaultId;
     }
 
-    function transferERC20(address tokenContractAddress, address recipient, uint256 amount) public {
-        require(_parentContract.isOwner(_vaultId), "only the owner of this vault's key can withdraw ERC20");
-        IERC20 tokenContract = IERC20(tokenContractAddress);
-        tokenContract.transfer(recipient, amount);
+    function transferERC20(IERC20 tokenContract, address recipient, uint256 amount) public {
+        require(_vaultKeyTokenContract.ownerOf(_vaultId) == msg.sender, "only the owner of this vault's key can withdraw ERC20");
+        tokenContract.transfer(recipient, amount); // This potentially passes control to an external contract
     }
 
-    function transferERC721(address tokenContractAddress, address recipient, uint256 tokenId) public {
-        require(_parentContract.isOwner(_vaultId), "only the owner of this vault's key can withdraw ERC721");
-        IERC721 tokenContract = IERC721(tokenContractAddress);
+    function transferERC721(IERC721 tokenContract, address recipient, uint256 tokenId) public {
+        require(_vaultKeyTokenContract.ownerOf(_vaultId) == msg.sender, "only the owner of this vault's key can withdraw ERC721");
         tokenContract.safeTransferFrom(address(this), recipient, tokenId); // This potentially passes control to an external contract
     }
 
     function transferETH(address payable recipient, uint256 amount) public {
-        require(_parentContract.isOwner(_vaultId), "only the owner of this vault's key can withdraw ETH");
+        require(_vaultKeyTokenContract.ownerOf(_vaultId) == msg.sender, "only the owner of this vault's key can withdraw ETH");
         recipient.transfer(amount); // This potentially passes control to an external contract
     }
 
