ebd76de7b3737c22f2e015db56b756723cd92550
Maximillian Laumeister <max@maxlaumeister.com>
2019-12-03 18:43:15 -0800

    Add automatic check-in functionality, update README.md

--- contracts/VaultManager.sol
+++ contracts/VaultManager.sol
@@ -21,8 +21,8 @@ contract VaultManager {
 
     constructor() public {
         // Global setup
-        vaultKeyTokenContract = new VaultAccessERC721(address(this));
-        vaultBeneficiaryTicketTokenContract = new VaultAccessERC721(address(this));
+        vaultKeyTokenContract = new VaultAccessERC721(this);
+        vaultBeneficiaryTicketTokenContract = new VaultAccessERC721(this);
     }
 
     function newVault() public {
@@ -34,9 +34,10 @@ contract VaultManager {
         vaults.push(VaultInfo(userVaultContract, 365 days, block.timestamp + 365 days)); // Save info about the vault
     }
 
-    // If you have the beneficiary ticket, when it's time, use your ticket to yank the vault key away from its current owner. Upon use, your beneficiary ticket will be locked in the vault.
+    // If you have the beneficiary ticket, when it's time, use your ticket to yank the vault key away from its current owner.
+    // Upon use, your beneficiary ticket will be locked in the vault for future reuse.
     function claimVaultKeyAsBeneficiary(uint256 vaultId) public {
-        require(_isAuthorizedBeneficiary(vaultId), "either the release time is in the future, or the sending account is not the beneficiary");
+        require(_isAuthorizedBeneficiary(vaultId), "either the release time is in the future, or you do not have the right beneficiary claim ticket");
         vaultKeyTokenContract.transferAuthorized(vaultKeyTokenContract.ownerOf(vaultId), msg.sender, vaultId); // Transfer the key from its current owner to msg.sender, the beneficiary.
         VaultInfo memory vault = vaults[vaultId];
         vaultBeneficiaryTicketTokenContract.transferAuthorized(msg.sender, address(vault.vaultContract), vaultId); // Transfer the benificiary claim ticket into the vault
@@ -46,11 +47,24 @@ contract VaultManager {
         require(vaultKeyTokenContract.ownerOf(vaultId) == msg.sender, "only owner can set check in interval");
         vaults[vaultId].checkInInterval = newCheckInInterval;
         // Automatically check in
-        vaults[vaultId].lastCheckIn = block.timestamp;
+        _checkIn(vaultId);
     }
 
     function checkIn(uint256 vaultId) public {
         require(vaultKeyTokenContract.ownerOf(vaultId) == msg.sender, "only owner can check in");
+            _checkIn(vaultId);
+    }
+
+    function autoCheckIn(uint256 vaultId, address sender) public {
+        require(address(vaultKeyTokenContract) == msg.sender
+            || address(vaultBeneficiaryTicketTokenContract) == msg.sender
+            , "only the authorized token contracts can call this");
+        if (vaultKeyTokenContract.ownerOf(vaultId) == sender) {
+            _checkIn(vaultId);
+        }
+    }
+
+    function _checkIn(uint256 vaultId) private {
         vaults[vaultId].lastCheckIn = block.timestamp;
     }
 
